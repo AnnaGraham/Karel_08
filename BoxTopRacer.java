dd0e2428e4af94d1e99c0b12e70d079b73bbf90e
AnnaGraham <anna_graham@asl.org>
2014-12-08 10:39:45 +0000

    Lab_08 check

--- BoxTopRacer.java
+++ BoxTopRacer.java
@@ -9,13 +9,34 @@ public class BoxTopRacer extends Racer {
 	
 	//TODO Subclasses do not inherit any constructors from their superclass
 	//Consequently, you will need to provide a compatible constructor
+	public BoxTopRacer(int y){
+		super(y);
+	}
 	
 	/**
 	 * Re-implement the method to allow for jumping hurdles of any width
 	 */
 	@Override
 	public void jumpRight() {
+		turnLeft();
+		int count = 0;
+		while(!rightIsClear()){
+			move();
+			count++;
+		}
 		
+		turnRight();
+		move();
+		while(!rightIsClear()){
+			move();
+		}
+		turnRight();
+		
+		for(int i = 0; i< count; i++){
+			move();
+			
+		}
+		turnRight();
 	}
 
 }
--- SteepleChaseRacer.java
+++ SteepleChaseRacer.java
@@ -1,3 +1,5 @@
+import edu.fcps.karel2.Display;
+
 /**
  * A SteepleChaseRacer is a Racer that can jump over hurdles of any height.
  * 
@@ -9,13 +11,29 @@ public class SteepleChaseRacer extends Racer {
 	
 	//TODO Subclasses do not inherit any constructors from their superclass
 	//Consequently, you will need to provide a compatible constructor
+	public SteepleChaseRacer(int y){
+		super(y);
+	}
 	
 	/**
 	 * Re-implement the method to allow for jumping hurdles of any height
 	 */
-	@Override
+	
 	public void jumpRight() {
-		
+		turnLeft();
+		int count = 0;
+		while(!rightIsClear()){
+			move();
+			count++;
+		}
+		turnRight();
+		move();
+		turnRight();
+		for(int i = 0; i< count; i++){
+			move();
+			
+		}
+		turnRight();
 	}
 
 }
